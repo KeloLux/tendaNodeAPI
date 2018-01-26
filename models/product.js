dd326dd2b15d882023b603266170fe2e0b2b0df0
mique <miquelcata@hotmail.es>
2018-01-26 15:52:37 +0100

    validation min max

--- models/product.js
+++ models/product.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const ProductSchema = new Schema({
-    name: String,
+    name: { type: String, required: true },
     picture: String,
-    price: { type: Number, default: 0 },
+    price: {
+        type: Number,
+        min: [1, 'Price has to be greater or equals than 1'],
+        max: 1000
+    },
     category: { type: String, enum: ['harware', 'software', 'maintenance'] },
     description: String
 });
 
-
-
-module.exports = mongoose.model('Product', ProductSchema)
+module.exports = mongoose.model('Product', ProductSchema);
